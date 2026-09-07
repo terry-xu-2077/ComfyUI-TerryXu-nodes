bab36ebbc6f25f4f099301d5739eb7b8fcdac530
Terry <49803399+terry-xu-2077@users.noreply.github.com>
2026-09-07 10:26:45 +0800

    Register pure boolean switch module

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,8 +1,10 @@
+from .boolean_switch import BooleanSwitch
 from .bool_switch import BoolSwitch
 from .h3_prompt_editor import H3PromptEditor
 from .line_switch import LineSwitch
 
 __all__ = [
+    "BooleanSwitch",
     "BoolSwitch",
     "H3PromptEditor",
     "LineSwitch",
