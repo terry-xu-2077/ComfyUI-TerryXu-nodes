8a652b5ae1d815674ac8f89e3e8d8b1e21bd7c72
Terry <49803399+terry-xu-2077@users.noreply.github.com>
2026-09-09 12:30:42 +0800

    Register remote control node module

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -2,10 +2,12 @@ from .boolean_switch import BooleanSwitch
 from .bool_switch import BoolSwitch
 from .h3_prompt_editor import H3PromptEditor
 from .line_switch import LineSwitch
+from .remote_control import RemoteControl
 
 __all__ = [
     "BooleanSwitch",
     "BoolSwitch",
     "H3PromptEditor",
     "LineSwitch",
+    "RemoteControl",
 ]
